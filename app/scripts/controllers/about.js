9485acce08c29730214243a8bbbd35fd57d20547
Dawn Milnamow <dawnMil.ZCW.de@gmail.com>
2015-10-30 19:44:48 -0400

    Created Angular App

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name team6App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the team6App
+ */
+angular.module('team6App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name team6App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the team6App
+ */
+angular.module('team6App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
