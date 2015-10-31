d69983ce69003167c2bb4dd77d8d5e88eb9bc393
Dawn Milnamow <dawnMil.ZCW.de@gmail.com>
2015-10-31 00:16:56 -0400

    Task Controller createTask methods added

---
+++ backend/src/main/java/team/six/controllers/TaskController.java
@@ -0,0 +1,35 @@
+package team.six.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import team.six.models.Task;
+import team.six.models.TaskDAO;
+
+/**
+ * Created by dmilnamow on 10/30/15.
+ */
+@Controller
+public class TaskController {
+
+    @Autowired
+    TaskDAO taskDAO;
+
+    @RequestMapping("/createTask")
+    @ResponseBody
+    public String createTask(String title, String description, Integer points, Integer listid){
+        Task task;
+        try{
+            task = new Task(title, description, points, 1);
+            taskDAO.save(task);
+        }catch(Exception ex){
+            return "Error creating task. Try again. ============================" + ex.toString();
+        }
+        return "Task successfully created! (id = " + task.getId() + ")";
+
+    }
+
+
+
+}
